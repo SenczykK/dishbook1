cb3b85ad9a6bdfea7d8e51679c38227227753544
SenczykK <senczyk.kamil@gmail.com>
2020-04-08 11:39:02 +0200

    Updates

---
+++ precache-manifest.c73aac0d3bfe92b0476079e5c2ff109f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "397995317b490becc1f9470173de94d5",
+    "url": "/dishbook1/index.html"
+  },
+  {
+    "revision": "bfc0454923e267ea126c",
+    "url": "/dishbook1/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "64cb9e8f5d59d694cfd9",
+    "url": "/dishbook1/static/js/2.b32850cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dishbook1/static/js/2.b32850cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfc0454923e267ea126c",
+    "url": "/dishbook1/static/js/main.58867edf.chunk.js"
+  },
+  {
+    "revision": "63985f0d37744103b6fb",
+    "url": "/dishbook1/static/js/runtime-main.1e9b6ebe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dishbook1/static/media/logo.5d5d9eef.svg"
+  }
+]);
